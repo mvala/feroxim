a78b519b23b4bf2fd5212c5d195b8a4d518f540c
Martin Vala <mvala@saske.sk>
2014-10-15 01:12:53 +0200

    Fix Makefile.in

--- cmake/modules/root-utils.cmake
+++ cmake/modules/root-utils.cmake
@@ -3,7 +3,7 @@ function(add_par PACKAGE)
   message(STATUS "Generating ${PACKAGE}")
   add_custom_target(${PACKAGE}.par
       COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_SOURCE_DIR}/pars/${PACKAGE}
-    COMMAND sed -e 's,include ..ROOTSYS./\\\(etc\\|test\\\)/Makefile.arch,include Makefile.arch,\; s/PACKAGE = */PACKAGE = ${PACKAGE}/\; s,SRCS *=.*,SRCS = ${SRCS},\;' < ${CMAKE_SOURCE_DIR}/cmake/pars/Makefile.in | sed -e 's,HDRS *=.*,HDRS = ${HDRS},\; s,FSRCS *=.*,FSRCS = ${FSRCS},\; s,DHDR *=.*,DHDR = ${DHDR},\; s,EINCLUDE *:=.*,EINCLUDE := ${saveEINCLUDE},' > ${CMAKE_SOURCE_DIR}/pars/${PACKAGE}/Makefile
+    COMMAND sed -e 's,include ..ROOTSYS./\\\(etc\\|test\\\)/Makefile.arch,include Makefile.arch,\; s/PACKAGE = */PACKAGE = ${PACKAGE}/\; s,SRCS *=.*,SRCS = ${SRCS},\;' < ${CMAKE_SOURCE_DIR}/cmake/par/Makefile.in | sed -e 's,HDRS *=.*,HDRS = ${HDRS},\; s,FSRCS *=.*,FSRCS = ${FSRCS},\; s,DHDR *=.*,DHDR = ${DHDR},\; s,EINCLUDE *:=.*,EINCLUDE := ${saveEINCLUDE},' > ${CMAKE_SOURCE_DIR}/pars/${PACKAGE}/Makefile
     COMMAND cp -pR `root-config --etcdir`/Makefile.arch ${CMAKE_SOURCE_DIR}/pars/${PACKAGE}/Makefile.arch
     COMMAND cp -pR ${CMAKE_SOURCE_DIR}/${PACKAGE}/* ${CMAKE_SOURCE_DIR}/pars/${PACKAGE}/
     COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_SOURCE_DIR}/pars tar --exclude=.svn -czhf ${CMAKE_SOURCE_DIR}/pars/${PACKAGE}.par ${PACKAGE}
